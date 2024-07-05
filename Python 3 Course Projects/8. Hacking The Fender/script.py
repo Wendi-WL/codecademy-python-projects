ca547ab6f22390b7f0b5edcd42a91df774aa3910
Wendi <liu.wendi@yahoo.ca>
2024-07-05 14:59:06 -0700

    fix relative path using script directory

--- Python 3 Course Projects/8. Hacking The Fender/script.py
+++ Python 3 Course Projects/8. Hacking The Fender/script.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import json
 
@@ -5,18 +6,33 @@ import json
 
 compromised_users = []
 
-with open("passwords.csv", "r") as password_file:
+# I used os to locate this script's directory and access the passwords csv file in it, after moving it off of codecademy.
+def full_path(relative_path):
+  script_dir = os.path.dirname(__file__)
+  return os.path.join(script_dir, relative_path)
+
+with open(full_path("passwords.csv"), "r") as password_file:
   password_csv = csv.DictReader(password_file)
   for row in password_csv:
     compromised_users.append(row['Username'])
 
-with open("compromised_users.txt", "w") as compromised_users_file: 
+# test
+with open(full_path("passwords.csv")) as pw:
+  passwords = pw.read()
+print(passwords)
+
+with open(full_path("compromised_users.txt"), "w") as compromised_users_file: 
   for user in compromised_users:
     compromised_users_file.write(user + "\n")
 
+# test
+with open(full_path("compromised_users.txt")) as cu:
+  users = cu.read()
+print(users)
+
 # Notifying the Boss
 
-with open("boss_message.json", "w") as boss_message:
+with open(full_path("boss_message.json"), "w") as boss_message:
   boss_message_dict = {
     "recipient": "The Boss", 
     "message": "Mission Success"
@@ -24,12 +40,12 @@ with open("boss_message.json", "w") as boss_message:
   json.dump(boss_message_dict, boss_message)
 
 # test
-with open("boss_message.json") as m:
-  message = m.read()
+with open(full_path("boss_message.json")) as bm:
+  message = bm.read()
 print(message)
 
 # Scrambling the Password
-with open("new_passwords.csv", "w") as new_passwords_obj:
+with open(full_path("new_passwords.csv"), "w") as new_passwords_obj:
   slash_null_sig = """ 
 / )( \   / __) /  \(_  _)            
 ) \/ (  ( (_ \(  O ) )(              
@@ -49,6 +65,6 @@ with open("new_passwords.csv", "w") as new_passwords_obj:
   new_passwords_obj.write(slash_null_sig)
 
 # test
-with open("new_passwords.csv") as new_passwords_obj:
-  new = new_passwords_obj.read()
-print(new)
+with open(full_path("new_passwords.csv")) as np:
+  new_passwords = np.read()
+print(new_passwords)
